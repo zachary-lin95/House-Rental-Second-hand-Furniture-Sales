f69527ed38a2445ce6242a1e93a2de4edfdb0c67
Ke Lin <lin.ke2@husky.neu.edu>
2018-04-17 20:05:05 -0400

    Initial Commit

---
+++ House Rental & Second-hand Furniture Sales/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  House Rental & Second-hand Furniture Sales
+//
+//  Created by 林轲 on 2018/4/17.
+//  Copyright © 2018年 Northeastern University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
