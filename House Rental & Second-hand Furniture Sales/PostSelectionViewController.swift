aa4a2850eb72b63c2aee9a709d954c8fa705b7f2
Ke Lin <lin.ke2@husky.neu.edu>
2018-04-19 20:48:15 -0400

    publish main view finished

--- House Rental & Second-hand Furniture Sales/PostSelectionViewController.swift
+++ House Rental & Second-hand Furniture Sales/PostSelectionViewController.swift
@@ -11,42 +11,20 @@ import UIKit
 
 class PostSelectionViewController: UIViewController {
     
-    @IBOutlet weak var textBtn: UIStackView!
-    @IBOutlet weak var mediaBtn: UIStackView!
-    @IBOutlet weak var topBtn: UIStackView!
-    @IBOutlet weak var checkinBtn: UIStackView!
-    @IBOutlet weak var dianpingBtn: UIStackView!
-    @IBOutlet weak var moreBtn: UIStackView!
+    @IBOutlet weak var RentalStackview: UIStackView!
+    @IBOutlet weak var FurnitureStackView: UIStackView!
     
-    @IBOutlet weak var cancelBtn: UIButton!
-    @IBAction func cancelBtnPressed(_ sender: UIButton) {
-        
-        
+    @IBOutlet weak var btnCanceled: UIButton!
+    @IBAction func btnCanceled(_ sender: Any) {
         let WINDOW_HEIGHT = self.view.frame.height
-        let BTN_HEIGHT = self.textBtn.frame.height
-        
+        let BTN_HEIGHT = self.RentalStackview.frame.height
         UIView.animate(withDuration: 0.7, delay: 0.5, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.textBtn.frame.origin.y = WINDOW_HEIGHT
+            self.RentalStackview.frame.origin.y = WINDOW_HEIGHT
         }, completion: nil)
         UIView.animate(withDuration: 0.7, delay: 0.3, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.mediaBtn.frame.origin.y = WINDOW_HEIGHT
+            self.FurnitureStackView.frame.origin.y = WINDOW_HEIGHT
         }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0.1, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.topBtn.frame.origin.y = WINDOW_HEIGHT
-        }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0.4, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.checkinBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
-        }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0.2, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.dianpingBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
-        }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.moreBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
-        }, completion: nil)
-        
-        dismiss(animated: true, completion: nil)
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
--- House Rental & Second-hand Furniture Sales/TabbarViewController.swift
+++ House Rental & Second-hand Furniture Sales/TabbarViewController.swift
@@ -10,20 +10,12 @@ import UIKit
 
 class TabBarViewController: UITabBarController {
     
-    var textBtn: UIStackView!
-    var mediaBtn: UIStackView!
-    var topBtn: UIStackView!
-    var checkinBtn: UIStackView!
-    var dianpingBtn: UIStackView!
-    var moreBtn: UIStackView!
+    var rentalBtn: UIStackView!
+    var furnitureBtn: UIStackView!
     var cancelBtn: UIButton!
     
-    var textBtnPosY: CGFloat!
-    var mediaBtnPosY: CGFloat!
-    var topBtnPosY: CGFloat!
-    var checkinBtnPosY: CGFloat!
-    var dianpingBtnPosY: CGFloat!
-    var moreBtnPosY: CGFloat!
+    var rentalBtnPosY: CGFloat!
+    var furnitureBtnPosY: CGFloat!
     
     var popView: UIView!
     var blurView: UIView!
@@ -39,15 +31,15 @@ class TabBarViewController: UITabBarController {
         let MARGIN_X = CGFloat(2)
         let MARGIN_Y = CGFloat(5)
         let BTN_WIDTH = TAB_HEIGHT - MARGIN_X * 2
-        let BTN_HEIGHT = TAB_HEIGHT - MARGIN_Y * 2
+        let BTN_HEIGHT = TAB_HEIGHT - MARGIN_Y * 2+8
         
         let modalView = UIView()
         modalView.frame = CGRect(x: GRID_WIDTH * 2, y: WINDOW_HEIGHT - TAB_HEIGHT, width: GRID_WIDTH, height: TAB_HEIGHT)
         self.view.addSubview(modalView)
         
         let postBtn = UIButton()
-        postBtn.frame = CGRect(x: GRID_WIDTH * 2 + (GRID_WIDTH - BTN_WIDTH) / 2, y: WINDOW_HEIGHT - TAB_HEIGHT + MARGIN_Y-30, width: BTN_WIDTH, height: BTN_HEIGHT)
-        postBtn.setBackgroundImage(UIImage(named: "earth"), for: UIControlState())
+        postBtn.frame = CGRect(x: GRID_WIDTH * 2 + (GRID_WIDTH - BTN_WIDTH) / 2, y: WINDOW_HEIGHT - TAB_HEIGHT + MARGIN_Y-39, width: BTN_WIDTH, height: BTN_HEIGHT)
+        postBtn.setBackgroundImage(UIImage(named: "add"), for: UIControlState())
         
         self.view.addSubview(postBtn)
         
@@ -69,24 +61,18 @@ class TabBarViewController: UITabBarController {
             self.blurView = blurView
             
             
-            let PSVC = self.storyboard?.instantiateViewController(withIdentifier: "PostSelection") as! PostSelectionViewController
+            let PSVC = self.storyboard?.instantiateViewController(withIdentifier: "PostSelectionViewController") as! PostSelectionViewController
             PSVC.modalTransitionStyle = .crossDissolve
             self.view.addSubview(PSVC.view)
             
-            self.textBtn = PSVC.textBtn
-            self.mediaBtn = PSVC.mediaBtn
-            self.topBtn = PSVC.topBtn
-            self.checkinBtn = PSVC.checkinBtn
-            self.dianpingBtn = PSVC.dianpingBtn
-            self.moreBtn = PSVC.moreBtn
-            self.cancelBtn = PSVC.cancelBtn
+            self.rentalBtn = PSVC.RentalStackview
+            self.furnitureBtn = PSVC.FurnitureStackView
+            self.cancelBtn = PSVC.btnCanceled
             
-            self.textBtnPosY = PSVC.textBtn.frame.origin.y
-            self.mediaBtnPosY = PSVC.mediaBtn.frame.origin.y
-            self.topBtnPosY = PSVC.topBtn.frame.origin.y
-            self.checkinBtnPosY = PSVC.checkinBtn.frame.origin.y
-            self.dianpingBtnPosY = PSVC.dianpingBtn.frame.origin.y
-            self.moreBtnPosY = PSVC.moreBtn.frame.origin.y
+            self.rentalBtnPosY = PSVC.RentalStackview.frame.origin.y
+            self.furnitureBtnPosY = PSVC.FurnitureStackView.frame.origin.y
+            
+            
             
             self.popView = PSVC.view
             
@@ -106,59 +92,32 @@ class TabBarViewController: UITabBarController {
         })
         
         let WINDOW_HEIGHT = self.view.frame.height
-        let BTN_HEIGHT = self.textBtn.frame.height
-        self.textBtn.frame.origin.y = WINDOW_HEIGHT
-        self.mediaBtn.frame.origin.y = WINDOW_HEIGHT
-        self.topBtn.frame.origin.y = WINDOW_HEIGHT
-        self.checkinBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
-        self.dianpingBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
-        self.moreBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
+        let BTN_HEIGHT = self.rentalBtn .frame.height
+        self.rentalBtn.frame.origin.y = WINDOW_HEIGHT
+        
         
         
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: [], animations: { () -> Void in
-            self.textBtn.frame.origin.y = self.textBtnPosY
+            self.rentalBtn.frame.origin.y = self.rentalBtnPosY
         }, completion: nil)
         UIView.animate(withDuration: 0.7, delay: 0.2, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: [], animations: { () -> Void in
-            self.mediaBtn.frame.origin.y = self.mediaBtnPosY
+            self.furnitureBtn.frame.origin.y = self.furnitureBtnPosY
         }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0.4, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: [], animations: { () -> Void in
-            self.topBtn.frame.origin.y = self.topBtnPosY
-        }, completion: nil)
-        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: [], animations: { () -> Void in
-            self.checkinBtn.frame.origin.y = self.checkinBtnPosY
-        }, completion: nil)
-        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: [], animations: { () -> Void in
-            self.dianpingBtn.frame.origin.y = self.dianpingBtnPosY
-        }, completion: nil)
-        UIView.animate(withDuration: 1, delay: 0.4, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: [], animations: { () -> Void in
-            self.moreBtn.frame.origin.y = self.moreBtnPosY
-        }, completion: nil)
+        
     }
     
     @objc func cancelBtnPressed(_ sender: UIButton) {
         
         
         let WINDOW_HEIGHT = self.view.frame.height
-        let BTN_HEIGHT = self.textBtn!.frame.height
+        let BTN_HEIGHT = self.rentalBtn!.frame.height
         
         
         UIView.animate(withDuration: 0.7, delay: 0.5, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.textBtn.frame.origin.y = WINDOW_HEIGHT
+            self.rentalBtn.frame.origin.y = WINDOW_HEIGHT
         }, completion: nil)
         UIView.animate(withDuration: 0.7, delay: 0.3, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.mediaBtn.frame.origin.y = WINDOW_HEIGHT
-        }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0.1, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.topBtn.frame.origin.y = WINDOW_HEIGHT
-        }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0.4, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.checkinBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
-        }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0.2, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.dianpingBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
-        }, completion: nil)
-        UIView.animate(withDuration: 0.7, delay: 0, options: UIViewAnimationOptions(), animations: { () -> Void in
-            self.moreBtn.frame.origin.y = WINDOW_HEIGHT + BTN_HEIGHT
+            self.furnitureBtn.frame.origin.y = WINDOW_HEIGHT
         }, completion: nil)
         UIView.animate(withDuration: 1.3, animations: { () -> Void in
             self.blurView.alpha = 0
